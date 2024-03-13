adeb4be5cf7ab2408e7c58ba1d0db2230495a1f5
unknown <achraf.abbes1@outlook.com>
2024-03-13 15:06:19 +0100

    det

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // fassarlyBaseUrl: 'https://fassarly.azurewebsites.net/fassarly'
-      fassarlyBaseUrl: 'http://102.211.210.98:8080/fassarly'
+      fassarlyBaseUrl: 'https://102.211.210.98:8080/fassarly'
     // fassarlyBaseUrl: 'http://localhost:8080/fassarly'
   };
